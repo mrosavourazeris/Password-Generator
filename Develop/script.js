26027297dacf23cfa249bfcf5e5d4302dcb410a7
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-11-21 19:28:40 -0600

    Added conditions to prompts if cancel is selected

--- Develop/script.js
+++ Develop/script.js
@@ -23,28 +23,47 @@ generateBtn.addEventListener("click", writePassword);
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password, THEN I am presented with a series of prompts for password criteria
 function generatePassword() {
-  // WHEN prompted for the length of the password, THEN I choose a length of at least 8 characters and no more than 128 characters
+  // promptOne is asking the user how long they would like their password to be 
   var promptOne = parseInt(prompt("How long would you like your password to be? Must be between 8 and 128 characters."))
+  
   if(!promptOne){
+    //if promptOne is not an integer then an alert will pop up notifying the user that they must pick a number between 8 and 128
     alert("You must enter a number between 8 and 128")
-    generatePassword()
+    //after the alert a confirm prompt will generate asking user if they want to cancel 
+    var askCancel = confirm("Would you like to cancel?")
+    //if askCancel is false, then the function generatePassword will end
+    if (askCancel === false){
+      return;
+    }
+    //if askCancel is True, the function generatePassword will run again from the beginning
+    else{
+    generatePassword()}
   }
+  
   else if(promptOne < 8 || promptOne >128 ){
+    //if the number picked is lower than 8 or higher than 128, an alert will pop up notifying the user that they must pick a number between 8 and 128
     alert("You must enter a number between 8 and 128")
-    generatePassword()
+    var askCancel = confirm("Would you like to cancel?")
+    //if askCancel is false, then the function generatePassword will end
+    if (askCancel === false){
+      return;
+    }
+    //if askCancel is True, the function generatePassword will run again from the beginning
+    else{
+    generatePassword()}
   }
   else {
-  var askUpper = prompt("Would you like to include uppercase letters in the password? Please enter Yes or No.")
-  var askLower = prompt("Would you like to include lowercase letters in the password? Please enter Yes or No.")
-  var askNumbers = prompt("Would you like to include numbers in the password? Please enter Yes or No.")
-  var askSpecial = prompt("Would you like to include special characters in the password? Please enter Yes or No.")
+  var askUpper = confirm("Would you like to include uppercase letters in the password?)
+  var askLower = confirm("Would you like to include lowercase letters in the password?)
+  var askNumbers = confirm("Would you like to include numbers in the password?)
+  var askSpecial = confirm("Would you like to include special characters in the password?)
   var askUpper2 = askUpper.toLocaleLowerCase()
   var askLower2 = askLower.toLocaleLowerCase()
   var askNumbers2 = askNumbers.toLocaleLowerCase()
   var askSpecial2 = askSpecial.toLocaleLowerCase()
   }
 
-  console.log(askUpper, askLower, askNumbers, askSpecial, askUpper2, askLower2, askNumbers2, askSpecial2)
+  console.log(promptOne, askUpper, askLower, askNumbers, askSpecial, askUpper2, askLower2, askNumbers2, askSpecial2)
 }
 
 
