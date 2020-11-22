d1dc80b31ee0c0aa018230c4666583ec8cb1badf
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-11-21 18:19:53 -0600

    added prompt to ask user to pick a password between 8 and 128 characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+//global variables
+
+var lowerLetter = "acdefghijklmnopqrstuvwxyz"
+var upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var theNumbers = "0123456789"
+var specialC = "!@#$^&%*()+=-[]{}|:<>?,.';"
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,11 +21,24 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
+// WHEN I click the button to generate a password, THEN I am presented with a series of prompts for password criteria
+function generatePassword() {
+  // WHEN prompted for the length of the password, THEN I choose a length of at least 8 characters and no more than 128 characters
+  var promptOne = prompt("How long would you like your password to be? Must be between 8 and 128 characters.")
+  if(promptOne === ""){
+    alert("You must enter a number between 8 and 128")
+    generatePassword()
+  }
+  else if(promptOne < 8 || promptOne >128 ){
+    alert("You must enter a number between 8 and 128")
+    generatePassword()
+  }
+  else {
+  console.log(promptOne)}
+}
+
+
+
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and/or special characters
